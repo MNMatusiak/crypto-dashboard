bab2c7f91532d32ecca1ae9a10a4f18f41fa2e61
MNMatusiak <m.n.matusiak@gmail.com>
2022-03-25 11:45:45 +0100

    Crypto list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import CryptoList from "./CryptoList";
 
 function App() {
-  return (
-    <div >
-      app
-    </div>
-  );
+    const [coins, setCoins] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get(
+                'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false'
+            )
+            .then(res => {
+                setCoins(res.data);
+                console.log(res.data);
+            })
+            .catch(error => console.log("Issue connecting with API"));
+    }, []);
+
+    return (
+        <div className='crypto-app'>
+
+            {coins.map(coin => {
+                return (
+                    <CryptoList key={coin.id} name={coin.name} price={coin.current_price} image={coin.image}
+                                symbol={coin.symbol}/>
+                );
+            })}
+
+        </div>
+    );
 }
 
 export default App;
