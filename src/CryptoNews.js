9b75e6e224974c970048056c5cce896fd93a74aa
MNMatusiak <m.n.matusiak@gmail.com>
2022-04-03 10:31:38 +0200

    Crypto News - commented out

--- src/CryptoNews.js
+++ src/CryptoNews.js
@@ -1,28 +1,28 @@
-import React, {useEffect, useState} from "react";
-import './CryptoNews.css';
-import axios from "axios";
-
-const CryptoNews = () => {
-
-    const [coins, setCoins] = useState([]);
-
-    useEffect(() => {
-        axios
-            .get(
-                'https://api.coingecko.com/api/v3/exchange_rates'
-            )
-            .then(res => {
-                setCoins(res.data);
-                console.log(res.data);
-            })
-            .catch(error => console.log("Issue connecting with API"));
-    }, []);
-
-    return (
-            <div className='page-main'>
-
-            </div>
-    )
-}
-
-export default CryptoNews;
+// import React, {useEffect, useState} from "react";
+// import './CryptoNews.css';
+// import axios from "axios";
+//
+// const CryptoNews = () => {
+//
+//     const [coins, setCoins] = useState([]);
+//
+//     useEffect(() => {
+//         axios
+//             .get(
+//                 'https://api.coingecko.com/api/v3/exchange_rates'
+//             )
+//             .then(res => {
+//                 setCoins(res.data);
+//                 console.log(res.data);
+//             })
+//             .catch(error => console.log("Issue connecting with API"));
+//     }, []);
+//
+//     return (
+//             <div className='page-main'>
+//
+//             </div>
+//     )
+// }
+//
+// export default CryptoNews;
